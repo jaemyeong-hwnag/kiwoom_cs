3aabc3fd13810d47b4473d0299d65cb35e38e434
jaemyeong-hwnag <woaud2sla@gmail.com>
2020-11-26 22:58:17 +0900

    테스트 작업 및 DB 작업

---
+++ KOASampleCS/Auoto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuotoSet
+{
+
+    public class AuotoCode
+    {/*
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI;
+        
+        public void oder(Object[] data) {
+            axKHOpenAPI.SendOrder(
+             data[0], // 사용자 구분명
+             data[1], // 화면번호
+             data[2],  // 계좌번호 10자리
+             data[3],  // 주문유형 1:신규매수, 2:신규매도 3:매수취소, 4:매도취소, 5:매수정정, 6:매도정정
+             data[4], // 종목코드
+             data[5],  // 주문수량
+             data[6], // 주문가격
+             data[7],   // 거래구분(혹은 호가구분)은 아래 참고
+             data[8]  // 원주문번호입니다. 신규주문에는 공백, 정정(취소)주문할 원주문번호를 입력합니다.
+             );
+        }*/
+    }
+
+    // 구분명, 주문유형 종목코드 주문수량 주문가격 03
+}
---
+++ KOASampleCS/DBInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace databaseMySQL
+{
+    class DBInfo
+    {
+        private MySqlConnection conn;
+        private MySqlCommand command;
+
+        public DBInfo() 
+        {
+            try {
+                conn = new MySqlConnection("");
+            } catch (Exception exception) {
+
+            }
+        }
+
+        public void Execute(string query) 
+        {
+            try {
+                conn.Open();
+                command = new MySqlCommand(query, conn);
+                conn.Close();
+            } catch (Exception exception) {
+                
+            }
+        }
+
+        public MySqlDataReader GetRowData(string query) 
+        {
+            conn.Open();
+            command = new MySqlCommand(query, conn);
+            MySqlDataReader row = command.ExecuteReader();
+
+            return row;
+        }
+    }
+}
