7653b8cc5020e99b9f6077f1d4454c1bf1c9b8db
jaemyeong-hwnag <woaud2sla@gmail.com>
2020-12-06 02:00:17 +0900

    DB SQL select관련 함수 추가, DB로그인정보 분리

--- KOASampleCS/DBInfo.cs
+++ KOASampleCS/DBInfo.cs
@@ -5,44 +5,78 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Diagnostics;
+using DBCommon;
 
 namespace databaseMySQL
 {
     class DBInfo
     {
-        private MySqlConnection conn;
-
-        public DBInfo() 
-        {
-            try {
-                this.conn = new MySqlConnection("");
-                this.conn.Open();
-            } catch (Exception exception) {
-                Debug.WriteLine(exception);
-            }
-        }
-
+        DBConnSet connSet = new DBConnSet();
         public void Execute(string query) 
         {
             try {
-                MySqlCommand command = new MySqlCommand(query, this.conn);
+                MySqlConnection conn = this.Connection();
+                conn.Open();
+                MySqlCommand command = new MySqlCommand(query, conn);
+                Debug.WriteLine(query);
                 command.ExecuteNonQuery();
+                Debug.WriteLine(query);
+                conn.Close();
             } catch (Exception exception) {
-                Debug.WriteLine(exception);
+                // Debug.WriteLine(exception);
             }
         }
 
-        public MySqlDataReader GetRowData(string query) 
+        public string GetRowData(string query) 
         {
-            MySqlCommand command = new MySqlCommand(query, this.conn);
+            MySqlConnection conn = this.Connection();
+            conn.Open();
+            //Debug.WriteLine(query);
+            MySqlCommand command = new MySqlCommand(query, conn);
+            MySqlDataReader rdr = command.ExecuteReader();
+
+            string temp = string.Empty;
+
+            if (rdr == null) temp = "No return";
+            else
+            {
+                while (rdr.Read())
+                {
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+                        if (i != rdr.FieldCount - 1)
+                            temp += rdr[i] + ";";    // parser 넣어주기
+                        else if (i == rdr.FieldCount - 1)
+                            temp += rdr[i] + "\n";
+                    }
+                }
+            }
+            //Debug.WriteLine(temp);
+
+            rdr.Close();
+            conn.Close();
+            return temp;
+        }
+        
+        public int RowCount(string query) 
+        {
+            MySqlConnection conn = this.Connection();
+            conn.Open();
+            MySqlCommand command = new MySqlCommand(query, conn);
             MySqlDataReader row = command.ExecuteReader();
 
-            return row;
+            row.Read();
+            int myCount = Convert.ToInt32(row["cnt"].ToString());
+            row.Close();
+            conn.Close();
+            /*Debug.WriteLine(myCount);
+            Debug.WriteLine(query);*/
+            return myCount;
         }
 
-        ~DBInfo() 
+        private MySqlConnection Connection() 
         {
-            this.conn.Close();
-        }
+            return new MySqlConnection(connSet.getConnectionStr());
+    }
     }
 }
